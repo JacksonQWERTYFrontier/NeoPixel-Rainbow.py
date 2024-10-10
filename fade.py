95c235e03b6f9fbd5cf33db7173b1aa419bd7785
JacksonQWERTYFrontier <2025remy.fritz@fcboe.org>
2024-10-10 13:36:52 -0400

    Update 2 fade.py

--- fade.py
+++ fade.py
@@ -6,16 +6,22 @@ import random
 np = NeoPixel(board.D2, 30, auto_write=False, brightness = 0.3)
 i = 0
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-cyan = (0, 255, 255)
-magenta = (255, 0, 255)
+for i in range(1,51)
+purple = (255, 111, 255)
+green = (144, 255, 144)
 
-color_list = [red, green, blue, yellow, cyan, magenta]
-while True:
-    for i in range(np.n):
-        np[i] = random.choice(color_list)
-        np.show()
-        time.sleep(0.1)
+
+color_list = [purple, green]
+def fade_in(color):
+    fade_ratio = color
+    
+def fade_out():
+    while True:
+        for i in range(np.n):
+            time.sleep(0.2)
+            np[i] = random.choice(color_list)
+            np.show()
+
+
+fade_out()
+fade_in()
