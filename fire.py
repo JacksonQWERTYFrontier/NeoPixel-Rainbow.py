b3d34c246871c4192ffb0b8ad0c3df406618d3db
JacksonQWERTYFrontier <2025remy.fritz@fcboe.org>
2024-10-22 13:35:49 -0400

    fire.py

--- fire.py
+++ fire.py
@@ -6,11 +6,11 @@ import random
 np = NeoPixel(board.D2, 31, auto_write=False, brightness = 0.8)
 
 def flames(pone, ptwo, pthree, pfour):
-        np.fill(pone)
+    np.fill(pone)
+    np.show()
+    for i in range(ptwo):
+        np[random.randint(0, np.n - 2)]= pthree
         np.show()
-        for i in range(ptwo):
-            np[random.randint(0, np.n - 2)]= pthree
-            np.show()
-            time.sleep(pfour)
+        time.sleep(pfour)
 while True:
     flames((250, 80, 0), 50, (200, 34, 0), .0000001)
