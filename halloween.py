5fb750a4956198991425676af3d42fc7f4264355
JacksonQWERTYFrontier <2025remy.fritz@fcboe.org>
2024-10-28 13:43:35 -0400

    halloween.py

--- halloween.py
+++ halloween.py
@@ -9,7 +9,7 @@ i = 0
 
 magenta = (255,0,255)
 white = (255,255,255)
-orange = (255, 135, 0)
+orange = (255, 135, 30)
 
 #    flames(magenta, 50, (200, 34, 0), .0000001)
 #    fade_in(orange, 0.01)
@@ -47,9 +47,9 @@ def fade_out(color,speed):
     np.show()
     ratio_orange = color[2]/50
     orig_orange = color[2]
-    ratio_magenta = color[0]/50
+    ratio_magenta = color[2]/50
     orig_magenta = color[0]
-    ratio_white = color[1]/50
+    ratio_white = color[2]/50
     orig_white = color[1]
     for i in range(1,51):
         r = int(orig_orange - i * ratio_orange)
@@ -66,7 +66,7 @@ while True:
     anything = random.choice(palet_list)
     for i in range(sparkleween):
         flames(anything, 50, (200, 34, 0), .0000001)
-    fade_in(anything, 0.01)
-    fade_out(white,.01)
+    fade_in(anything, 0.006)
+    fade_out(magenta,.006)
     for i in range(sparkleween):
         flames(anything, 50, (200, 34, 0), .0000001)
